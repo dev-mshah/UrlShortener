1aded4894b91c84488bd2f35c58636c0d5e2027b
user <user@users-mbp.mynetworksettings.com>
2026-03-12 00:16:36 -0400

    Added core logic within Url Service

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
 
 using TinyUrlService.Data;
 using TinyUrlService.Utils;
+using TinyUrlService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<Postgres>();
 builder.Services.AddSingleton<Redis>();
 builder.Services.AddSingleton<SqidsGenerator>();
+builder.Services.AddSingleton<UrlService>();
 
 var app = builder.Build();
 
